489b60b9f45a86ed4f047e50463d948500ef746c
gamerraggy <javib0414@gmail.com>
2025-01-08 09:21:50 -0700

    Create 2nddailycode.py

---
+++ 2nddailycode.py
@@ -0,0 +1,13 @@
+#javi
+#01/08/25
+#time: 9:16 AM - 9:20 AM #i forgot to do the second one
+goatqbs = ["tom brady", "bo nix", "patrick mahomes"]
+print(goatqbs)
+print("Let me add one more")
+goatqbs.append("peyton manning")
+print (goatqbs)
+goatqbs.sort()
+print(goatqbs)
+print("Let me make the list better")
+goatqbs.remove("patrick mahomes")
+print(goatqbs)
