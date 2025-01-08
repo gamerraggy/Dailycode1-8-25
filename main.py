4f482e2ad22eeae32ef0b7f9a54830a7fb73cb66
gamerraggy <javib0414@gmail.com>
2025-01-08 09:02:15 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#javi
+#01/08/25
+#time: 8:49 AM - 8:59 AM 
+#while loop to print the numbers 0-10 counting by 2s
+num = 0 #starting point
+while num<11: #go one above 10 so it prints to 10
+    print(num, end = " ")
+    num += 2 #what you count by
+    
+#while loop to print the numbers 50-0 counting by 5s
+print(" ") #ends line
+num2 = 50 #starting point
+while num2>-5: #go below 0 so it prints to 0
+    print(num2, end = " ")
+    num2 -= 5 #what you count by
+    
+#while loop to print "bruh" until the user types "jeremy stop"
+print(" ") #ends line
+UserInput = "bqabdj" #dummy value
+while UserInput != "jeremy stop":
+    UserInput = input("bruh")
+    
+#while loop to print "Whats your favorite football team?" until the user types "broncos"
+print(" ") #ends line
+UserInput2 = "bqabdj" #dummy value
+while UserInput2 != "broncos":
+    UserInput2 = input("Whats your favorite football team?")
+    
